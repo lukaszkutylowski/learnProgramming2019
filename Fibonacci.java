c653b3436eac6598ce77ba35c51ab46f239f0b06
Lukasz Kutylowski <kutylowski.lukasz.1@gmail.com>
2018-11-23 20:22:54 +0100

    program upgrade - maths operation realized in other file

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,5 +1,5 @@
 /*
-Fibonacci serie - defined by array 10 elements; program can show a
+Fibonacci serie - defined by array 10 elements; program send values of variables to Function class; program can show a
 selected number of serie for example position three: 1
 */
 
@@ -8,18 +8,13 @@ import java.util.Scanner;
 public class Fibonacci {
 	public static void main(String[] args) {
 	
-	int fibonacciSeries[] = {0,1,1,2,3,5,8,13,21,34};
-	
-	System.out.println("write a number in serie (1...10): ");
+	System.out.print("write a number in serie (1...10): ");
 	Scanner input = new Scanner(System.in);
 	
 	int intUserInput = input.nextInt();
-	int serieArrayNumber = intUserInput - 1;
 	
-	System.out.print("value of " + intUserInput + " fibonacci serie: ");
-	System.out.println(fibonacciSeries[serieArrayNumber]);
-	
-	System.out.print("value of " + intUserInput + " fibonacci series: ");
-	System.out.println(fibonacciSeries[intUserInput]);
+	Function sendInput = new Function();
+	sendInput.setInput(intUserInput);
+	sendInput.getInput();
 	}
 }
