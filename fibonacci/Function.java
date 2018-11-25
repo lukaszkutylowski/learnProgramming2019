78dbf7727c9d43f637d9dc829dbdb887e0a6c384
Lukasz Kutylowski <kutylowski.lukasz.1@gmail.com>
2018-11-25 18:30:29 +0100

    new file

---
+++ fibonacci/Function.java
@@ -0,0 +1,21 @@
+/* Function catch value of variables and realized print of fibonacci 
+series number */
+
+import java.util.Scanner;
+
+public class Function {
+	
+	int fibonacciSeries[] = {0,1,1,2,3,5,8,13,21,34};
+	private int localIntUserInput;
+	private int serieArrayNumber;
+	private int var;
+	
+	public void setInput(int intUserInput) {
+		localIntUserInput = intUserInput;
+		serieArrayNumber = localIntUserInput - 1;
+	}
+	public void getInput() {
+		System.out.print("value of "+localIntUserInput+" fibonacci serie: ");
+		System.out.println(fibonacciSeries[serieArrayNumber]);
+	}
+}
