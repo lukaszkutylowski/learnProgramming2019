473b96bef7ef7eb18f0ccc506e04a2d8a9583b06
Lukasz Kutylowski <kutylowski.lukasz.1@gmail.com>
2018-12-02 15:21:10 +0100

    functions separated from main function in other file

--- exercisePPJ_06_I/ExercisePPJ_06_I.java
+++ exercisePPJ_06_I/ExercisePPJ_06_I.java
@@ -17,42 +17,13 @@ public class ExercisePPJ_06_I {
 	byte wrt = input.nextByte();
 	
 	double wrtDouble = (double) wrt;
-
-	for(int counter = 7; counter >= 0; counter--) {
-		
-		int exponent = 0;
-		
-		while(wrtDouble > Math.pow(2,exponent)) {
-			exponent++;
-		}
-		
-		if(wrtDouble == Math.pow(2,exponent)) {
-			binaryWrt[exponent] = Math.pow(2,exponent);
-		}
-		else {
-			binaryWrt[exponent-1] = Math.pow(2,(exponent-1));
-			
-			int highestExponent = exponent-1;
-			double rest = wrtDouble - Math.pow(2,highestExponent);
-			
-			wrtDouble = rest;
-			exponent=0;
-		}
-	}
 	
-	for(int exponent = 0; exponent <= 7; exponent++) {
-		System.out.println("2^"+exponent+": "+binaryWrt[exponent]);
-	}
+	FunctionPPJ_06_I sendParam = new FunctionPPJ_06_I();
+	sendParam.setFunction(wrtDouble);
+	sendParam.getFunction();
+	sendParam.showFunction();
+	sendParam.binaryFunction();
 	
-	System.out.print("Binary: 0b");
-	for(int exponent = 7; exponent >= 0; exponent--) {
-		if(binaryWrt[exponent] != 0.0) {
-			System.out.print("1");
-		}
-		else {
-			System.out.print("0");
-		}
-	}
 	System.out.println("");
 	}
 }
---
+++ exercisePPJ_06_I/FunctionPPJ_06_I.java
@@ -0,0 +1,52 @@
+public class FunctionPPJ_06_I {
+	
+	private double binaryWrt[] = {0,0,0,0,0,0,0,0};
+	private double wrtDouble = 0;
+	
+	public void setFunction(double wrtD) {
+		wrtDouble = wrtD;
+	}
+	
+	public void getFunction() {
+		
+		for(int counter = 7; counter >= 0; counter--) {
+		
+		int exponent = 0;
+		
+		while(wrtDouble > Math.pow(2,exponent)) {
+			exponent++;
+		}
+		
+		if(wrtDouble == Math.pow(2,exponent)) {
+			binaryWrt[exponent] = Math.pow(2,exponent);
+		}
+		else {
+			binaryWrt[exponent-1] = Math.pow(2,(exponent-1));
+			
+			int highestExponent = exponent-1;
+			double rest = wrtDouble - Math.pow(2,highestExponent);
+			
+			wrtDouble = rest;
+			exponent=0;
+		}
+	}
+
+		}
+	public void showFunction() {
+		for(int exponent = 0; exponent <= 7; exponent++) {
+		System.out.println("2^"+exponent+": "+binaryWrt[exponent]);
+		}
+	}
+	
+	public void binaryFunction() {
+		System.out.print("Binary: 0b");
+		for(int exponent = 7; exponent >= 0; exponent--) {
+			if(binaryWrt[exponent] != 0.0) {
+				System.out.print("1");
+			}
+			else {
+				System.out.print("0");
+			}
+		}
+	}
+}
