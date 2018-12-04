de19cbaa09ec0a23c672cca53e37a5dfbf2e547a
Lukasz Kutylowski <kutylowski.lukasz.1@gmail.com>
2018-12-04 12:22:04 +0100

    delete a useless variable

--- PrimeNumberNaive/FunctionNaive.java
+++ PrimeNumberNaive/FunctionNaive.java
@@ -8,7 +8,6 @@ public class FunctionNaive {
 	private double result=0;
 	private double number;
 	private double copyResult;
-	private double floorDoubleResult;
 	private int floorResult;
 
 	public void setFunction(double num) {
@@ -19,8 +18,7 @@ public class FunctionNaive {
 	while(result <= (number - 1)) {
 		result = number / startValue;
 		copyResult = result;
-		floorDoubleResult = Math.floor(copyResult);
-		floorResult = (int) floorDoubleResult;
+		floorResult = (int) Math.floor(copyResult);
 		
 		if(startValue == (number - 1)) {
 			System.out.println("YES it is a prime number");
