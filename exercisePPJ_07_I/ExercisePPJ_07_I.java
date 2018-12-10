84a6b72c1bf242029ba6e1381d6ae36d94654b95
Lukasz Kutylowski <kutylowski.lukasz.1@gmail.com>
2018-12-10 16:53:55 +0100

    program show range of input number

---
+++ exercisePPJ_07_I/ExercisePPJ_07_I.java
@@ -0,0 +1,21 @@
+/*
+Dana jest zmienna int wrt zainicjowana dowolną wartością liczbową z przedziału 0 do 10.
+Utwórz program wypisujący na ekranie "int z przedziału <6, 10>" gdy zmienna wrt jest większa od 5,
+oraz "int z przedziału <0, 5>"w przeciwnym przypadku.
+*/
+
+import java.util.Scanner;
+
+public class ExercisePPJ_07_I {
+	public static void main(String[] args) { 
+		System.out.print("Input value in range 0...10: ");
+		Scanner input = new Scanner(System.in);
+		int wrt = input.nextInt();
+		
+		if(wrt > 5) {
+			System.out.println("int z przedziału <6, 10>");
+		} else {
+			System.out.println("int z przedziału <0, 5>");
+		}
+	}
+}
