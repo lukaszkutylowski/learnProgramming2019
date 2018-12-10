28f214fb1082d7f339eee267e6b271fba0e4097f
Lukasz Kutylowski <kutylowski.lukasz.1@gmail.com>
2018-12-10 17:12:13 +0100

    new skill - input char ASCII to variable from keyboard

---
+++ exercisePPJ_07_II/ExercisePPJ_07_II.java
@@ -0,0 +1,20 @@
+/*
+Dana jest zmienna char chr zainicjowana dowolną wartością znakową z przedziału A do Z.
+Utwórz program wypisujący na ekranie „znak A - G” gdy zmienna chr jest mniejsza od G, oraz „znak H - Z” w przeciwnym przypadku.
+*/
+
+import java.util.Scanner;
+
+public class ExercisePPJ_07_II {
+	public static void main(String[] args) {
+		System.out.print("Input a big letter from range A...Z: ");
+		Scanner input = new Scanner(System.in);
+		char chr = input.next().charAt(0);
+		
+		if(chr <= 71) {
+			System.out.println("Znak A - G");
+		} else {
+			System.out.println("Znak G - Z");
+		}
+	}
+}
