7c77d2955598402b944302f60622b4916f51eff7
Lukasz Kutylowski <kutylowski.lukasz.1@gmail.com>
2018-12-12 21:57:37 +0100

    text to variable, indentations ok

--- exercisePPJ_06_IV/FunctionPPJ_06_IV.java
+++ exercisePPJ_06_IV/FunctionPPJ_06_IV.java
@@ -8,31 +8,32 @@ public class FunctionPPJ_06_IV {
 	}
 	
 	public void calculateDays() {
+		String text = "From 1st January to this day passage of time is: ";
 		switch(fMonth) {
-		case 1:
-		System.out.println("From 1st January to this day passage of time is: "+fDay+" days"); break;
-		case 2:
-		System.out.println("From 1st January to this day passage of time is: "+(31+fDay)+" days"); break;
-		case 3:
-		System.out.println("From 1st January to this day passage of time is: "+(59+fDay)+" days"); break;
-		case 4:
-		System.out.println("From 1st January to this day passage of time is: "+(90+fDay)+" days"); break;
-		case 5:
-		System.out.println("From 1st January to this day passage of time is: "+(120+fDay)+" days"); break;
-		case 6:
-		System.out.println("From 1st January to this day passage of time is: "+(151+fDay)+" days"); break;
-		case 7:
-		System.out.println("From 1st January to this day passage of time is: "+(181+fDay)+" days"); break;
-		case 8:
-		System.out.println("From 1st January to this day passage of time is: "+(212+fDay)+" days"); break;
-		case 9:
-		System.out.println("From 1st January to this day passage of time is: "+(242+fDay)+" days"); break;
-		case 10:
-		System.out.println("From 1st January to this day passage of time is: "+(273+fDay)+" days"); break;
-		case 11:
-		System.out.println("From 1st January to this day passage of time is: "+(303+fDay)+" days"); break;
-		case 12:
-		System.out.println("From 1st January to this day passage of time is: "+(334+fDay)+" days"); break;
-	}
+			case 1:
+			System.out.println(text+fDay+" days"); break;
+			case 2:
+			System.out.println(text+(31+fDay)+" days"); break;
+			case 3:
+			System.out.println(text+(59+fDay)+" days"); break;
+			case 4:
+			System.out.println(text+(90+fDay)+" days"); break;
+			case 5:
+			System.out.println(text+(120+fDay)+" days"); break;
+			case 6:
+			System.out.println(text+(151+fDay)+" days"); break;
+			case 7:
+			System.out.println(text+(181+fDay)+" days"); break;
+			case 8:
+			System.out.println(text+(212+fDay)+" days"); break;
+			case 9:
+			System.out.println(text+(242+fDay)+" days"); break;
+			case 10:
+			System.out.println(text+(273+fDay)+" days"); break;
+			case 11:
+			System.out.println(text+(303+fDay)+" days"); break;
+			case 12:
+			System.out.println(text+(334+fDay)+" days"); break;
+		}
 	}
 }
