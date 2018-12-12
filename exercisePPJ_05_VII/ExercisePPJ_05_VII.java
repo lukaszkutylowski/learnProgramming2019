a91d215fd4303f6d94e03f9bb9af28e8b1b57070
Lukasz Kutylowski <kutylowski.lukasz.1@gmail.com>
2018-12-12 22:21:37 +0100

    show result procesure changed

--- exercisePPJ_05_VII/ExercisePPJ_05_VII.java
+++ exercisePPJ_05_VII/ExercisePPJ_05_VII.java
@@ -15,47 +15,42 @@ public class ExercisePPJ_05_VII {
 	
 	boolean czyPada = true;
 	boolean czySwieciSlonce = true;
+	String weather = "";
 	
 	System.out.println("Weather");
-	System.out.println("Please enter one from dwo numbers: (1) pada / (2) nie pada");
+	System.out.println("Please enter one from two numbers: (1) pada / (2) nie pada");
 	
 	Scanner input1 = new Scanner(System.in);
 	int a = input1.nextInt();
 
-	System.out.println("Please enter one form dwo numbers: (3) swieci / (4) nie swieci");
+	System.out.println("Please enter one form two numbers: (3) swieci / (4) nie swieci");
 
-	Scanner input2 = new Scanner(System.in);
-	int b = input2.nextInt();
+	//Scanner input2 = new Scanner(System.in);
+	int b = input1.nextInt();
 	
-	if(a == 1) {
-		czyPada = true;
-	}
-	else {
+	if(a != 1) {
 		czyPada = false;
 	}
 	
-	if(b == 3) {
-		czySwieciSlonce = true;
-	}
-	else {
+	if(b != 3) {
 		czySwieciSlonce = false;
 	}
 	
 	if((czyPada == true) && (czySwieciSlonce == false)) {
-		System.out.println("Weather is: Plucha");
+		weather = "Plucha";
 	}
 	
 	if((czyPada == true) && (czySwieciSlonce == true)) {
-		System.out.println("Weather is: Tecza");
+		weather = "Tecza";
 	}
 	
 	if((czyPada == false) && (czySwieciSlonce == false)) {
-		System.out.println("Weather is: Pochmurno");
+		weather = "Pochmurno";
 	}
 	
 	if((czyPada == false) && (czySwieciSlonce == true)) {
-		System.out.println("Weather is: Slonecznie");
+		weather = "Slonecznie";
 	}
-	
+	System.out.println("Weather is: "+weather);
 	}
 }
