10ef808ddd1b374aa174853e4bd0552e0cc63915
Lukasz Kutylowski <kutylowski.lukasz.1@gmail.com>
2018-12-12 21:31:24 +0100

    input modified - args

--- exercisePPJ_07_I/ExercisePPJ_07_I.java
+++ exercisePPJ_07_I/ExercisePPJ_07_I.java
@@ -8,10 +8,14 @@ import java.util.Scanner;
 
 public class ExercisePPJ_07_I {
 	public static void main(String[] args) { 
-		System.out.print("Input value in range 0...10: ");
-		Scanner input = new Scanner(System.in);
-		int wrt = input.nextInt();
+		//System.out.print("Input value in range 0...10: ");
+		//Scanner input = new Scanner(System.in);
+		//int wrt = input.nextInt();
 		
+		//code change - input from args
+		
+		String wrtS =  args[0];
+		int wrt = Integer.parseInt(wrtS);
 		if(wrt > 5) {
 			System.out.println("int z przedziału <6, 10>");
 		} else {
