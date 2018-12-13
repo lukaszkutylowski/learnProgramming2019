f9ffc7543201fd2d8ac52db8ab846eb917252a2e
Lukasz Kutylowski <kutylowski.lukasz.1@gmail.com>
2018-12-13 21:59:02 +0100

    modified test 'variable++'

--- plusPlus/PlusPlus.java
+++ plusPlus/PlusPlus.java
@@ -1,19 +1,22 @@
 public class PlusPlus {
 	public static void main(String[] args) {
-		int a = 1;
-		int b = 10;
-		int c = 20;
-		int d = 30;
-		int e = 40;
+		int a = 1, b = 1, c = 1, d = 1, e = 1;
 		
-		System.out.println(a);
-		b = a++;
-		System.out.println(a);
-		c = ++a;
-		System.out.println(a);
-		d = a++ + ++a;
-		System.out.println(a);
-		e = a + 1;
-		System.out.println(a);
+		System.out.println("start value of a = " + a);
+		b = b + a++;
+		System.out.println("start value of b = 1 and after' b = b + a++': " + b);
+		c = c + ++a;
+		System.out.println("start value of c = 1 and after' c = c + ++a': " + c);
+		d = d + a++ + ++a;
+		System.out.println("start value of d = 1 and after' d = d + a++ + ++a': " + d);
+		e = e + a + 1;
+		System.out.println("start value of e = 1 and after' e = e + a + 1': " + e);
+		
+		a = 1;
+		System.out.println("a++ :" + a++);
+		a = 1;
+		System.out.println("++a :" + ++a);
+		a = 1;
+		System.out.println("a++ + ++a :" + (a++ + ++a));
 	}
 }
