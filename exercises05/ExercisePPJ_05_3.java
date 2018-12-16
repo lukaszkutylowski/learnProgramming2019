85d566d5cad71d82947013d9cf476efa8d3d06f4
Mateusz Dzieiński <mdzielinski@gmail.com>
2018-12-16 11:05:52 +0100

    refactoring od code

--- exercises05/ExercisePPJ_05_3.java
+++ exercises05/ExercisePPJ_05_3.java
@@ -17,14 +17,10 @@ public class ExercisePPJ_05_3 {
 		Scanner input = new Scanner(System.in);
 		int wrt = input.nextInt();
 		
-		if((wrt > -15) && (wrt < -13)) {
-			System.out.println("variable wrt it is contained in interval A nad B");
-		} else if((wrt > -13) && (wrt < -10)) {
-				System.out.println("variable wrt it is contained in interval A only");
-			} else if(wrt < -15) {
-					System.out.println("variable wrt it is contained in interval B only");
-			 	} else {
-						System.out.println("variable wrt it is NOT contained in A and B intervals");
-			   		}
+		if((wrt <= -15) || ((wrt >= -13) && (wrt <= -10))) {
+			System.out.println(true);
+		} else {
+			System.out.println(false);
+		}
 	}
 }
